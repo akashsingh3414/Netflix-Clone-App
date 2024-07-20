5b40b975acde3dc5b1ee3710ccf3f104d4a95197
akashsingh3414 <akashsingh3414@gmail.com>
2024-07-21 02:57:19 +0530

    Update Firebase hooks in src/hooks/firebase.js
    - Refactored code for better readability and maintainability

--- src/hooks/firebase.js
+++ src/hooks/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxJ8WCtSm9DUoD9ZSGRrH8jD3xFovoHRE",
-  authDomain: "netflix-clone-project-9202e.firebaseapp.com",
-  projectId: "netflix-clone-project-9202e",
-  storageBucket: "netflix-clone-project-9202e.appspot.com",
-  messagingSenderId: "1030200783136",
-  appId: "1:1030200783136:web:3f93f0e3ae930e84bb2745"
+  apiKey: "yourAPIkey",
+  authDomain: "yourAuthDOmain",
+  projectId: "yourProjectID",
+  storageBucket: "yourStorageBucket",
+  messagingSenderId: "yourSenderID",
+  appId: "yourAppID"
 };
 
 const app = initializeApp(firebaseConfig);
