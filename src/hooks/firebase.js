71e2bbbf74bb08e5f4e53fc21c1213c1f5a22959
akashsingh3414 <akashsingh3414@gmail.com>
2024-07-21 10:07:20 +0530

    update firebase.js

--- src/hooks/firebase.js
+++ src/hooks/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } f
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "yourAPIkey",
-  authDomain: "yourAuthDOmain",
-  projectId: "yourProjectID",
-  storageBucket: "yourStorageBucket",
-  messagingSenderId: "yourSenderID",
-  appId: "yourAppID"
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
 };
 
 const app = initializeApp(firebaseConfig);
