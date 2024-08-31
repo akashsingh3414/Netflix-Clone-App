eeb7a4b1df42f5ca1ba41ee4adf52c61e3ead8cb
akashsingh3414 <akashsingh3414@gmail.com>
2024-09-01 02:07:41 +0530

    removed unimportant file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import Homepage from './components/Homepage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginScreen from './components/LoginScreen';
